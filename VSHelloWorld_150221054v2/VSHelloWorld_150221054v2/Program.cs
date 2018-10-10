c536b6b953f82da8b57d0c974a347e04dbdad9d5
ZerkOnic <150221054@estudantes.ips.pt>
2018-10-10 19:47:17 +0100

    teste

--- VSHelloWorld_150221054v2/VSHelloWorld_150221054v2/Program.cs
+++ VSHelloWorld_150221054v2/VSHelloWorld_150221054v2/Program.cs
@@ -15,7 +15,7 @@ namespace VSHelloWorld_150221054v2
         public static void Main(string[] args)
         {
             //Comment teste
-            //novo comentario
+            //novo comentario +1
             BuildWebHost(args).Run();
         }
 
